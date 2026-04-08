01687bd737132c864bc56f0159ad6d7c0c2c6d68
Daniil Potapov <190094519+Daniil-9@users.noreply.github.com>
2026-04-08 07:59:25 +0300

    Add files via upload

---
+++ Глава 5/get_page.py
@@ -0,0 +1,9 @@
+cache = {}
+
+def get_page(url):
+    if url in cache:
+        return cache[url]
+    else:
+        data = get_data_from_server(url)
+        cache[url] = data
+        return data
